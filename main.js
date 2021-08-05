ebfd853559a904550ebc396eac2ccc2107fb44dc
Kevin <kevin03.nguyen@gmail.com>
2021-08-05 18:03:51 -0400

    added walking and inline to the workout options in the form

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+let map, mapEvent;
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -20,7 +22,7 @@ if (navigator.geolocation) {
         `https://www.google.ca/maps/place/Jane+and+Finch,+Toronto,+ON/@${latitude},${longitude}`
       );
 
-      const map = L.map("map").setView([latitude, longitude], 13);
+      map = L.map("map").setView([latitude, longitude], 13);
 
       L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
@@ -32,10 +34,52 @@ if (navigator.geolocation) {
         .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
         .openPopup();
 
-      map.on();
+      map.on("click", function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+      });
     },
     function () {
       alert("Could not get your position");
     }
   );
 }
+
+//display marker
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // clear input
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      "";
+
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: `${inputType.value}-popup`,
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+});
+
+inputType.addEventListener("change", function () {
+  console.log(inputType.value);
+  if (inputType.value === "cycling") {
+    inputElevation.closest(".form__row").classList.remove("form__row--hidden");
+    inputCadence.closest(".form__row").classList.add("form__row--hidden");
+  } else {
+    inputElevation.closest(".form__row").classList.add("form__row--hidden");
+    inputCadence.closest(".form__row").classList.remove("form__row--hidden");
+  }
+});
